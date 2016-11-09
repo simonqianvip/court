c9ee5b6fb479888268bca0845a1c806535b29aea
simonqianvip <simonqian_vip@163.com>
2016-11-09 17:39:28 +0800

    数据持久化到mysql数据库

--- court/pipelines.py
+++ court/pipelines.py
@@ -18,11 +18,13 @@ class CourtPipeline(object):
         return item
 
 
-class MySQLStoreFmPipeline(object):
+logger = logging.getLogger(__name__)
+
+
+class MySQLStoreCourtPipeline(object):
     """
     数据存储到mysql
     """
-
     def __init__(self, dbpool):
         self.dbpool = dbpool
 
@@ -44,3 +46,32 @@ class MySQLStoreFmPipeline(object):
         )
         dbpool = adbapi.ConnectionPool('MySQLdb', **dbargs)
         return cls(dbpool)
+
+    # pipeline默认调用
+    def process_item(self, item, spider):
+        deferred = self.dbpool.runInteraction(self._do_insert, item, spider)
+        deferred.addErrback(self._handle_error)
+        # d.addBoth(lambda _: item)
+        return deferred
+
+    # 将每行更新或写入数据库中
+    def _do_insert(self, conn, item, spider):
+        """
+        CREATE TABLE `court_info` (
+          `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+          `title` varchar(100) COMMENT "标题",
+          `c_name` varchar(100) COMMENT '法院名称',
+          `number` varchar(100)  COMMENT '案件编号',
+          `c_date` VARCHAR(50)  COMMENT '日期',
+          `link` VARCHAR(200)  COMMENT 'url',
+          PRIMARY KEY (`id`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+        """
+        conn.execute("""
+                insert into court_info(title, c_name, number, c_date, link)
+                values(%s, %s, %s, %s ,%s)
+                """, (item['title'],item['c_name'] ,item['number'],item['c_date'],item['link']))
+        logger.info('insert into success')
+
+    def _handle_error(self, failue):
+        logger.error(failue)
